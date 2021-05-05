a1756631d22ef22944fa01a3e5108246f7a8bae6
System_Error <83075991+Systemanic@users.noreply.github.com>
2021-05-05 17:23:07 +0200

    Updated Create file.py

--- ERD Test/Create_txt.py
+++ ERD Test/Create_txt.py
@@ -1,6 +1,7 @@
 import os.path
 
-answer_file_rep = ["DBF_" + str(number) for number in range(4, 16)]
+ran = 16
+answer_file_rep = ["DBF_" + str(number) for number in range(ran)]
 num = 6
 while answer_file_rep:
     for files in answer_file_rep:
